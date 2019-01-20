79bf90411a6eb714506e2debfd630403e8fb5567
I324158 <I324158@INLN34386807A.apj.global.corp.sap>
2019-01-20 15:27:05 +0530

    commit

--- test/src/test/testSoumya.java
+++ test/src/test/testSoumya.java
@@ -5,11 +5,7 @@ public class testSoumya {
 	public static void main(String[] args) {
 		System.out.println("soumya");
 		System.out.println("Siku");
-<<<<<<< HEAD
-		System.out.println("Second branch");
-=======
-		System.out.println("Branchout few display");
->>>>>>> refs/heads/FewDisplay
+
 	}
 
 }
